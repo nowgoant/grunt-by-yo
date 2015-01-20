4ca2c066649cb9d7d16d438cafb741b142f87b8e
majun <631359926@qq.com>
2015-01-20 19:10:21 +0800

    init site

---
+++ app/scripts/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+define(function(require, exports) {
+    var touch = require('touch');
+
+    console.log(touch);
+
+    touch.on('.btn-success', 'tap', function() {
+        console.log(this);
+    });
+});
